757a7c57db065bd12e1f07389a8b565e177c074e
moko <ljun925@naver.com>
2025-05-24 12:06:54 +0900

    SSR, CSR client 분리

--- libs/supabase.ts
+++ libs/supabase.ts
@@ -1,8 +1,58 @@
-import { createBrowserClient } from '@supabase/ssr';
+import {
+  createBrowserClient,
+  createServerClient,
+  parseCookieHeader,
+  serializeCookieHeader,
+} from '@supabase/ssr';
+import { NextRequest } from 'next/server';
+import { MergeDeep } from 'type-fest';
+import { Database as SupabaseDatabase } from '@/database.types';
 
-export default function supabaseClient() {
-  return createBrowserClient(
+type Database = MergeDeep<SupabaseDatabase, Record<string, never>>;
+
+export const client = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export const ssrClient = (request: NextRequest) => {
+  const headers = new Headers();
+  const serverSideClient = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          try {
+            const cookies = parseCookieHeader(
+              request.headers.get('Cookie') ?? ''
+            );
+            return cookies.map(({ name, value }) => ({
+              name,
+              value: value ?? '',
+            }));
+          } catch (error) {
+            console.error('supabase: 쿠키 파싱 중 오류 발생:', error);
+            return [];
+          }
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            try {
+              headers.append(
+                'Set-Cookie',
+                serializeCookieHeader(name, value, options)
+              );
+            } catch (error) {
+              console.error(`supabase: 쿠키 설정 중 오류 발생: ${name}`, error);
+            }
+          });
+        },
+      },
+    }
   );
-}
+  return {
+    client: serverSideClient,
+    headers,
+  };
+};
