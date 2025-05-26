aa65dd65418937389dc2fd18717fde653cc115b7
moko <ljun925@naver.com>
2025-05-26 20:47:57 +0900

    gcp client filename -> credentials 으로 변경

--- libs/google-vision.ts
+++ libs/google-vision.ts
@@ -1,9 +1,5 @@
 import { ImageAnnotatorClient } from '@google-cloud/vision';
 
 export const googleVisionClient = new ImageAnnotatorClient({
-  keyFilename: process.env.GOOGLE_CLOUD_KEY_PATH,
-});
-
-export const ClientVision = new ImageAnnotatorClient({
-  keyFilename: process.env.NEXT_PUBLIC_GOOGLE_CLOUD_KEY_PATH,
+  credentials: JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS || '{}'),
 });
