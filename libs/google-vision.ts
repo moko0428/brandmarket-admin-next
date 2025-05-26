bb869b4814daac8b6553388c9d2bf6048aabb1b2
moko <ljun925@naver.com>
2025-05-26 21:15:14 +0900

    JSON key file 파싱

--- libs/google-vision.ts
+++ libs/google-vision.ts
@@ -1,5 +1,10 @@
 import { ImageAnnotatorClient } from '@google-cloud/vision';
 
 export const googleVisionClient = new ImageAnnotatorClient({
-  credentials: JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS || '{}'),
+  credentials: {
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY,
+    client_id: process.env.GCP_CLIENT_ID,
+    private_key_id: process.env.GCP_PRIVATE_KEY_ID,
+  },
 });
