473211d90a26e332a1a7a38d39136c414ee08958
moko <ljun925@naver.com>
2025-06-11 15:16:08 +0900

    홈 화면 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+import { Hero } from '@/common/components/hero';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      {/* <Link href="/product">Product &rarr;</Link> */}
-      <Link href="/location">Location &rarr;</Link>
+      <Hero
+        title="Brand Market"
+        subtitle="Brand Market에 오신 것을 환영합니다."
+      />
+      <div className="block md:hidden w-full h-full">
+        <div className="flex justify-center items-center h-full">
+          <div className="underline hover:text-blue-500">
+            <Link href="/location">
+              브랜드마켓 매장 위치 확인하러 가기 &rarr;
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
