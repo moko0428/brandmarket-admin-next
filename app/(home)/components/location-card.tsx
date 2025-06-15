99e013422bb9f8b74e6a2b367f4cbf233f5901db
moko <ljun925@naver.com>
2025-06-15 23:11:33 +0900

    반응형을 고려한 매장 리스트 크기 조정

--- app/(home)/components/location-card.tsx
+++ app/(home)/components/location-card.tsx
@@ -19,27 +19,27 @@ export default function LocationCard({
   distance: string;
 }) {
   return (
-    <div className="flex flex-col gap-2 border-2 border-black/50 mb-2">
+    <div className="grid grid-cols-2 gap-2 border-b border-black/30 last:border-b-0">
       <div key={id}>
         {image ? (
           <ImagePair image={image} name={name} />
         ) : (
           <Skeleton className="w-full h-70" />
         )}
-        <div className="p-2 space-y-2">
-          <div className="flex flex-col">
-            <span className="font-bold">{name}</span>
-            <span className="text-sm text-foreground/50">{address}</span>
-          </div>
-          <div className="flex items-center gap-2">
-            {isOpen ? (
-              <span className="text-sm text-green-500">영업중</span>
-            ) : (
-              <span className="text-sm text-red-500">영업 종료</span>
-            )}
-            <span className="text-sm">{openTime}</span>
-            <span className="text-sm">{distance}</span>
-          </div>
+      </div>
+      <div className="p-2 space-y-2 ">
+        <div className="flex flex-col space-y-1">
+          <span className="text-sm text-foreground">{name}</span>
+          <span className="text-xs text-foreground/50">{address}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          {isOpen ? (
+            <span className="text-sm text-green-500">영업중</span>
+          ) : (
+            <span className="text-sm text-red-500">영업 종료</span>
+          )}
+          <span className="text-xs text-foreground/70">{openTime}</span>
+          <span className="text-xs text-foreground/70">({distance})</span>
         </div>
       </div>
     </div>
