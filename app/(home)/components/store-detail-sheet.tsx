6aefb3959181fedb363e51f7d36284a39edcfc4a
moko <ljun925@naver.com>
2025-06-15 23:27:01 +0900

    반응형을 고려한 매장 디테일 화면 크기 조절

--- app/(home)/components/store-detail-sheet.tsx
+++ app/(home)/components/store-detail-sheet.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/common/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,7 +10,6 @@ import {
   SheetDescription,
   SheetHeader,
   SheetTitle,
-  SheetTrigger,
 } from '@/common/components/ui/sheet';
 import { useState } from 'react';
 import { Check, ChevronDown, ChevronUp, Copy } from 'lucide-react';
@@ -43,7 +41,6 @@ export default function StoreDetailSheet({
   return (
     <div className="hidden">
       <Sheet open={isOpen} onOpenChange={onClose}>
-        <SheetTrigger>Open</SheetTrigger>
         <SheetContent side="right" className="w-full h-[100dvh] p-0">
           <SheetHeader className="px-4 py-6 border-b sticky top-0 bg-white">
             <SheetTitle>{detailStore?.name}</SheetTitle>
@@ -51,7 +48,7 @@ export default function StoreDetailSheet({
           </SheetHeader>
 
           <div className="px-4 py-6 overflow-y-auto h-[calc(100dvh-80px)]">
-            <div className="aspect-video rounded-lg overflow-hidden bg-muted mb-6">
+            <div className="rounded-lg overflow-hidden mb-6">
               {detailStore?.image ? (
                 <ImagePair
                   image={detailStore?.image || ''}
@@ -64,14 +61,20 @@ export default function StoreDetailSheet({
 
             <div className="space-y-4">
               <div>
-                <h3 className="font-semibold mb-2">영업 시간</h3>
-                <p>{detailStore?.openTime}</p>
+                <h3 className="font-semibold mb-2 text-sm text-foreground">
+                  영업 시간
+                </h3>
+                <p className="text-xs text-foreground/70">
+                  {detailStore?.openTime}
+                </p>
               </div>
               <div>
-                <h3 className="font-semibold mb-2">주소</h3>
+                <h3 className="font-semibold mb-2 text-sm text-foreground">
+                  주소
+                </h3>
                 <DropdownMenu open={open} onOpenChange={openAdressMenu}>
                   <DropdownMenuTrigger className="flex items-center">
-                    <Button variant="ghost">{detailStore?.address}</Button>
+                    <p className="flex items-center">{detailStore?.address}</p>
                     {open ? (
                       <ChevronUp className="w-4 h-4" />
                     ) : (
@@ -83,9 +86,11 @@ export default function StoreDetailSheet({
                       <DropdownMenuItem
                         key={copy.copy_road}
                         onClick={() => handleCopy(copy.copy_road)}
-                        className="flex items-center justify-between gap-2 cursor-pointer"
+                        className="flex items-center justify-between gap-2 cursor-pointer hover:bg-transparent"
                       >
-                        <p>{copy.copy_road}</p>
+                        <p className="text-xs text-foreground/70">
+                          {copy.copy_road}
+                        </p>
                         {copied === copy.copy_road ? (
                           <Check className="w-4 h-4 text-green-500" />
                         ) : (
@@ -99,7 +104,9 @@ export default function StoreDetailSheet({
                       }
                       className="flex items-center justify-between gap-2 cursor-pointer"
                     >
-                      <p>{detailStore?.copy[0].copy_street}</p>
+                      <p className="text-xs text-foreground/70">
+                        {detailStore?.copy[0].copy_street}
+                      </p>
                       {copied === detailStore?.copy[0].copy_street ? (
                         <Check className="w-4 h-4 text-green-500" />
                       ) : (
@@ -110,10 +117,12 @@ export default function StoreDetailSheet({
                 </DropdownMenu>
               </div>
               <div>
-                <h3 className="font-semibold mb-2">오시는 길</h3>
+                <h3 className="font-semibold mb-2 text-sm text-foreground">
+                  오시는 길
+                </h3>
                 <div className="space-y-2">
                   {detailStore?.place.map((place) => (
-                    <p key={place} className="text-sm">
+                    <p key={place} className="text-xs text-foreground/70">
                       {place}
                     </p>
                   ))}
