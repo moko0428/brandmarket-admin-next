03bd5395022d5b503d0b501faf15af2519e5e5b4
moko <ljun925@naver.com>
2025-07-21 15:30:22 +0900

    매장 관리하기 UI 변경

--- app/(users)/profile/components/section/store-manage-section.tsx
+++ app/(users)/profile/components/section/store-manage-section.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/common/components/ui/button';
-import { StoreIcon } from 'lucide-react';
+import { ChevronRight, StoreIcon } from 'lucide-react';
 
 export default function StoreManageSection({
   setOpen,
@@ -7,14 +7,13 @@ export default function StoreManageSection({
   setOpen: (open: boolean) => void;
 }) {
   return (
-    <div className="flex flex-col items-center gap-2">
-      <Button
-        onClick={() => setOpen(true)}
-        variant={'link'}
-        className="flex flex-col"
-      >
-        <StoreIcon className="size-12" />
-        매장
+    <div className="flex items-center justify-between gap-2 border-b pb-2">
+      <div className="flex items-center gap-2">
+        <StoreIcon className="size-8" />
+        매장 관리하기
+      </div>
+      <Button onClick={() => setOpen(true)} variant={'link'}>
+        <ChevronRight className="w-5 h-5 text-gray-400" />
       </Button>
     </div>
   );
